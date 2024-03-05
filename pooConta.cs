a5125dd3b7c72cfa141c94e7400fed46acc06769
Gustavo Coelho Reis <70582963+Gustavo-gcr@users.noreply.github.com>
2024-03-05 16:28:25 -0300

    Transferência Bancaria C#

---
+++ pooConta.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1_POO
+{
+    internal class Conta
+    {
+        Stack<string> pilha = new Stack<string>();//cria a pilha
+
+        public Conta(string nome, string CPF, double saldo, int idConta)//construtor
+        {
+            this.Nome = nome;
+            this.CPF = CPF;
+            this.saldo = saldo;
+            this.idConta = idConta;
+        }
+        private string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private double saldo;
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+        private int idConta;
+
+        public int IdConta
+        {
+            get { return idConta; }
+            set { idConta = value; }
+        }
+
+        private string cpf;
+
+        public string CPF
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+        //fim dos gets/sets
+        public void Saque(double saque)//metodo saque
+        {
+            double limite = 100;
+            double maximo = saldo + limite;
+            if (saque > saldo)//saque maior q saldo
+            {
+
+                Console.WriteLine("Saldo insuficiente porém foi retirado do seu limite R$" + limite + " ,no seu proximo depoisito será descontado 3%");
+                if (saque > maximo)//saque maior q saldo+limite
+                {
+                    Console.WriteLine("O maximo que podera ser sacado é de R$" + maximo);
+
+                    Console.WriteLine("Deseja sacar essa contia da conta?\n (sim) (outra) (nao)");
+                    string ler = Console.ReadLine();
+                    ler = ler.ToLower();
+                    if (ler == "sim")//aceitar o saque limite + saldo
+                    {
+                        double total = limite * -1;
+                        Console.WriteLine("valor sacado:" + maximo);
+                        this.saldo = total;
+                        Console.WriteLine("Saldo atual da conta R$" + total);
+                        pilha.Push(this.Nome + " saldo R$" + this.saldo);
+                    }
+                    else if (ler == "outra")//outro valor 
+                    {
+                        Console.WriteLine("Qual o novo valor a ser sacado?");
+                        double valor = double.Parse(Console.ReadLine());
+                        if (valor < limite + this.saldo)
+                        {
+                            while (valor > maximo)
+                            {
+                                Console.WriteLine("Não sera aceito valor mais alto, escreva novamente");
+                                valor = double.Parse(Console.ReadLine());
+                            }
+                            Console.WriteLine("Valor sacado R$" + valor);
+                            pilha.Push(this.Nome + " sacou R$" + this.saldo);
+                            this.saldo = maximo - valor;
+                            Console.WriteLine("Saldo atual da conta R$" + this.saldo);
+
+                        }
+
+                    }
+                    else//nao foi efetuado saque
+                        Console.WriteLine("Não foi efetuado novo saque");
+
+
+                }
+                else//saque menor que saldo+limite, porem maior que saldo apenas
+                    this.saldo = (this.saldo + limite) - saque;
+                this.saldo = this.saldo * -1;
+                Console.WriteLine("Saldo autal da conta R$" + this.saldo);
+                pilha.Push(this.Nome + " sacou R$" + this.saldo);
+
+            }
+            else//saldo maior que saque
+            {
+                double total = this.saldo - saque;
+                Console.WriteLine("Saque no valor de R$" + saque + " realizado, saldo total de " + total);
+                pilha.Push(this.Nome + " saldo R$" + total);
+                this.saldo = total;
+            }
+
+        }
+
+        public void Deposito()//metodo deposito
+        {
+            if (this.saldo < 0)
+            {
+                double taxa = (this.saldo * 0.03);
+                double total = taxa + this.saldo;
+                Console.WriteLine("Você está negativado e terá que pagar uma taxa");
+                Console.WriteLine("Valor da taxa R$" + taxa + " ,valor total a ser pago R$" + total);
+                Console.WriteLine("Deseja pagar? (sim/não)");
+                string ler = Console.ReadLine().ToLower();
+
+                if (ler == "sim")//taxa paga, saldo zerado
+                {
+                    this.saldo = 0;
+                    pilha.Push(this.Nome + " depositou R$" + this.saldo);
+                    Console.WriteLine("Taxa paga com sucesso.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Qual o valor do depósito?");//novo deposito
+                int valor = int.Parse(Console.ReadLine());
+                this.saldo = valor;
+                pilha.Push(this.Nome + " depositou R$" + this.saldo);
+                Console.WriteLine("Depósito de R$" + valor + " realizado com sucesso.");
+
+            }
+        }
+
+        public void Extrato()//metodo extrato
+        {
+           
+            if (pilha.Count > 0)// se a pilha nao estiver vazia o metodo irá funcionar
+            {
+                Console.WriteLine("Ultima operação realizada na conta");
+                Console.WriteLine(pilha.Peek());
+                Console.WriteLine("Ultimas 10 operações na conta");
+                Stack<string> novapilha = new Stack<string>(pilha);
+                for (int i = 0; i < pilha.Count; i++)
+                {
+                    if (i == 9)
+                    {
+                        i = pilha.Count;
+                    }
+                    Console.WriteLine(novapilha.Peek());
+                    novapilha.Pop();
+
+                }
+            }
+               
+            else
+                Console.WriteLine("Não há operações na conta");
+
+            
+        }
+
+    }
+}
---
+++ pooMain.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1_POO
+{
+    internal class Program
+    {
+        static int GerarIdConta()//gera id aleatorio pra conta
+        {
+            Random random = new Random();
+            return random.Next(00001, 100000);
+        }
+        static Conta CriarConta()//cria conta
+        {
+            int numIdConta = GerarIdConta();
+            Console.WriteLine("Escreva seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Escreva seu CPF");
+            string CPF = Console.ReadLine();
+            Console.WriteLine("Seu id será " + numIdConta);
+            Console.WriteLine("Insira seu saldo");
+            double saldo = double.Parse(Console.ReadLine());
+            Conta conta = new Conta(nome, CPF, saldo, numIdConta);
+            return conta;
+        }
+        static Conta PesquisarConta(List<Conta> lista)//pesquisa conta
+        {
+            Console.WriteLine("Voce deseja buscaar a conta pelo CPF ou idConta?");//pesquisa pelo cppf ou id
+            string op = Console.ReadLine();
+            op = op.ToLower();
+            if (op == "cpf")
+            {
+                Console.WriteLine("Escreva o CPF:");
+                string cpf = Console.ReadLine();
+
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (lista[i].CPF == cpf)
+                    {
+                        return lista[i];
+                    }
+                }
+            }
+            else if (op == "idconta")
+            {
+                Console.WriteLine("Escreva o idConta:");
+                int idConta = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (lista[i].IdConta == idConta)
+                    {
+                        return lista[i];
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("conta inválida");
+            }
+
+            Console.WriteLine("Conta não encontrada.");
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            List<Conta> lista = new List<Conta>();//cria a lista de contas
+            string op;
+            do//repetição da aplicação
+            {
+
+                Console.WriteLine("\nOla seja bem vindo!\nVocê deseja criar uma conta ou pesquisar uma existente?\nResonda com (criar)ou(pesquisar)");
+                string ler = Console.ReadLine();
+                ler = ler.ToLower();
+
+
+                if (ler == "criar")//chama o metodo criar e adiciona na lista
+                {
+                    Conta conta = CriarConta();
+                    lista.Add(conta);
+                }
+                else if (ler == "pesquisar")//chama o metodo pesquisar
+                {
+                    Conta conta = PesquisarConta(lista);
+                    if (conta != null)
+                    {
+                        Console.WriteLine("Deseja fazer um deposito ou saque?\nResponda(deposito)(saque)");
+                        ler = Console.ReadLine();
+                        ler = ler.ToLower();
+                        if (ler == "deposito")// deposita na conta
+                        {
+                            conta.Deposito();
+                        }
+                        else if (ler == "saque")
+                        {
+                            Console.WriteLine("Quanto você deseja sacar?");
+                            double saque = double.Parse(Console.ReadLine());
+                            conta.Saque(saque);//saca na conta
+                        }
+                        else
+                            Console.WriteLine("Opção Inválida");
+
+
+
+                        Console.WriteLine("Deseja ver o extrato da sua conta?\n (sim) (nao)");
+                        ler = Console.ReadLine();
+                        ler = ler.ToLower();
+                        if (ler == "sim")//chama o extrato
+                        {
+                            conta.Extrato();
+                        }
+                    }
+                    else
+                        Console.WriteLine("Não é possivel usar uma conta que não tenha sido criada");
+
+                }
+
+                Console.WriteLine("Deseja continuar a pesquisar ou a criar?\n (sim) (nao)");
+                op = Console.ReadLine();
+                op = op.ToLower();
+            } while (op != "nao");
+            //fim do programa
+            Console.WriteLine("Fim do programa!");
+            Console.ReadKey();
+        }
+    }
+
+}
